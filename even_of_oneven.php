0495152bd1de45f4146c6d94a902b6c8bec44ca2
Candace <2100978@talnet.nl>
2020-01-16 14:21:49 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,15 @@
+<?php
+
+        $evenOneven = readline("Vul een getal in");
+
+        if (($evenOneven % 2) == 1)
+        { 
+            echo "$evenOneven is een oneven getal" ;
+        }
+            
+            if (($evenOneven % 2) == 0)
+                {
+                     echo "$evenOneven is eeen even getal" ;
+                }
+            
+    ?>          
